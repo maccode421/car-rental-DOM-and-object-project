e59433f30ebb3ee7c5364267a77181dbc68b7053
Macc <maccavalli@gmail.com>
2017-11-22 00:11:46 +0000

    try different codes

--- cars.js
+++ cars.js
@@ -1,33 +1,9 @@
 var carRental = { 
     "name": "Enterprise Car Rental",
-    ecoCars: 100,
+    ecoCars: 50,
     ecoCarsRented: 0,
-    midCars: 100,
+    midCars: 50,
     midCarsRented: 0
-};
-
-var rental = {
-    renters: [
-        {
-            firstName: "John",
-            lastName: "Doe"
-        },
-        {
-            firstName: "Jane",
-            lastName: "Fonda"
-        }
-    ],
-    
-    carType: [
-        {
-            type: "economy",
-            price: "$19.99"
-        },
-        {
-            type: "midsize",
-            price: "$29.99"
-        }
-    ]
 };
 
 function button() {
@@ -52,30 +28,58 @@ function button2() {
     button();
 }
 
-function carOption() {
-    var cars = document.getElementById("car-type").value;
-    var economy = rental.carType[0].price;
-    var midsize = rental.carType[1].price;
-    document.getElementById("displayInfo").innerHTML = "You selected: " + cars + " - " + economy;
-
-    document.getElementById("ecoCar").innerHTML = carRental.ecoCars;
-    document.getElementById("ecoCarRent").innerHTML = carRental.ecoCarsRented;
-    document.getElementById("ecoCarAvail").innerHTML = carRental.ecoCars - carRental.ecoCarsRented;
+var rental = {
+    carType: [
+        {
+            type: "economy",
+            price: "$19.99"
+        },
+        {
+            type: "midsize",
+            price: "$29.99"
+        }
+    ],
     
-    // document.getElementById("displayInfo").innerHTML = "You selected: " + cars + " - " + rental.carTypes[1].price;
-    
-    // document.getElementById("midCar").innerHTML = carRental.midCars;
-    // document.getElementById("midCarRent").innerHTML = carRental.midCarsRented;
-    // document.getElementById("midCarAvail").innerHTML = carRental.midCars - carRental.midCarsRented;
+    renters: []
+};
 
+function carOption(index) {
+    var selectCar = document.getElementById("selectType").value;
+    console.log(selectCar);
+    var economy = rental.carType[0].type + " - " + rental.carType[0].price;
+    var midsize = rental.carType[1].type + " - " + rental.carType[1].price;
+    console.log("hello yall!");
+    
+    if (selectCar !== "midsize") {
+        document.getElementById("displayInfo").innerHTML = "You selected: " + economy;
+        document.getElementById("ecoCarAvail").innerHTML = carRental.ecoCars - carRental.ecoCarsRented;
+        console.log("it works");
+    } 
+    else {
+        document.getElementById("displayInfo").innerHTML = "You selected: " + midsize;
+        document.getElementById("midCarAvail").innerHTML = carRental.midCars - carRental.midCarsRented;
+        console.log("it works too");
+    }
+    
+
+    // for (var i = 0; i < rental.carType.length; i++) {
+    //     console.log("start", i);
+    //     var carOption = document.createElement("OPTION");
+    //     carOption.setAttribute("value", "economy");
+    //     carOption.setAttribute("value", "midsize");
+    //     console.log("stop");
+        
+    // }
+    
+}
 
     // if (economy !== "none" ) {
     //     return midsize;
     // }
-    // button1();
-}
 
-function submit() {
-    carRental.midCarsAvail--;
-    carRental.midCarsRented++;
-}
+// function submit() {
+//     // carRental.midCarsAvail--;
+//     // carRental.midCarsRented++;
+//     button1();
+//     console.log('form submitted');
+// }
